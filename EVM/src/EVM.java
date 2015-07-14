83526335f53c68f67167239134c46d3e5cc67186
amitgude <gudeamit148@gmail.com>
2015-07-14 16:01:24 -0700

    basic structure 1

---
+++ EVM/src/EVM.java
@@ -0,0 +1,43 @@
+
+public class EVM {
+	BallotUnit ballotUnit = new BallotUnit();
+	ControlUnit controlUnit = new ControlUnit();
+	private Integer numberOfCandidates;
+	private Integer candidateNumber;
+	
+	public void setUpElection(int numberOfCandidates) {
+		// TODO Auto-generated method stub
+		this.numberOfCandidates= numberOfCandidates;
+	}
+
+	
+	public void startPoll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void vote(int candidateNumber) {
+		// TODO Auto-generated method stub
+		this.candidateNumber = candidateNumber;
+	}
+
+
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void declareResult() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
--- EVM/src/Main.java
+++ EVM/src/Main.java
@@ -3,7 +3,25 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		EVM evm = new EVM();
+		
+		// set up election
+		evm.setUpElection(3);
+		
+		// start the poll
+		evm.startPoll();
+		
+		//vote
+		evm.vote(2);
+		
+		//close the poll
+		evm.close();
+		
+		//result
+		evm.declareResult();
+		
+		//clear
+		evm.clear();
 	}
 
 }
